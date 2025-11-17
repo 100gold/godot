xianofantasia/Go

change allow_hidpi project setting to hidpi_awareness. Allow to use PER_MONITOR_DPI_AWARE option for application

platform/windows/display_server_windows.cpp
@@ -6134,7 +6134,8 @@ DisplayServerWindows::DisplayServerWindows(const String &p_rendering_driver, Win
 
 	tablet_drivers.push_back("dummy");
 
-	if (OS::get_singleton()->is_hidpi_allowed()) {
+	OS::HidpiAwareness awareness_setting = OS::get_singleton()->get_hidpi_awareness();
+	if (awareness_setting != OS::HidpiAwareness::NO_AWARENESS) {
 		HMODULE Shcore = LoadLibraryW(L"Shcore.dll");
 
 		if (Shcore != nullptr) {
@@ -6143,7 +6144,11 @@ DisplayServerWindows::DisplayServerWindows(const String &p_rendering_driver, Win
 			SetProcessDpiAwareness_t SetProcessDpiAwareness = (SetProcessDpiAwareness_t)GetProcAddress(Shcore, "SetProcessDpiAwareness");
 
 			if (SetProcessDpiAwareness) {
-				SetProcessDpiAwareness(SHC_PROCESS_SYSTEM_DPI_AWARE);
+				if (awareness_setting == OS::HidpiAwareness::PER_MONITOR_AWARENESS) {
+					SetProcessDpiAwareness(SHC_PROCESS_PER_MONITOR_DPI_AWARE);
+				} else {
+					SetProcessDpiAwareness(SHC_PROCESS_SYSTEM_DPI_AWARE);
+				}
 			}
 		}
 	}

100gold/godot

platform/windows/display_server_windows.cpp
@@ -6922,8 +6922,13 @@ DisplayServerWindows::DisplayServerWindows(const String &p_rendering_driver, Win
 		}
 	}
 
-	if (OS::get_singleton()->is_hidpi_allowed()) {
-		SetProcessDpiAwareness(PROCESS_SYSTEM_DPI_AWARE);
+	OS::HidpiAwareness awareness_setting = OS::get_singleton()->get_hidpi_awareness();
+	if (awareness_setting != OS::HidpiAwareness::NO_AWARENESS) {
+		if (awareness_setting == OS::HidpiAwareness::PER_MONITOR_AWARENESS) {
+			SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+		} else {
+			SetProcessDpiAwareness(PROCESS_SYSTEM_DPI_AWARE);
+		}
 	}
 
 	HMODULE comctl32 = LoadLibraryW(L"comctl32.dll");